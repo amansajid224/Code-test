2bb44ef8fc5624c1928a121110182a9d89947960
Aman Sajid <amansajid.upwork@gmail.com>
2023-12-30 01:23:05 +0000

    extra data removed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -51,12 +51,6 @@ const Login = () => {
                             className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
-                    <Link
-                        href="/forget"
-                        className="text-xs text-blue-600 hover:underline"
-                    >
-                        Forget Password?
-                    </Link>
                     <div className="mt-2">
                         <button
                             className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
